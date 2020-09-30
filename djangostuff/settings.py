aa8af1909555afbcd22d1eebb3bdb05b256634ee
Daniel Jere <danieljere@ymail.com>
2020-09-30 06:23:12 -0700

    tweaked settings

--- djangostuff/settings.py
+++ djangostuff/settings.py
@@ -34,8 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+   'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -46,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
